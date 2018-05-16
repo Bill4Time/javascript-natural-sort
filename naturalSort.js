cec70cf43fcc87fff9a7f576a94fd7277dccec2a
Eric Brody <eric@kitcheck.com>
2018-05-16 09:11:56 -0400

    If a value is null or undefined treat it like an empty string

--- naturalSort.js
+++ naturalSort.js
@@ -10,7 +10,7 @@ module.exports = function naturalSort (a, b) {
 		dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
 		hre = /^0x[0-9a-f]+$/i,
 		ore = /^0/,
-		i = function(s) { return naturalSort.insensitive && ('' + s).toLowerCase() || '' + s; },
+		i = function(s) { return (s === null || s === undefined) ? '' : (naturalSort.insensitive && ('' + s).toLowerCase() || '' + s); },
 		// convert all to strings strip whitespace
 		x = i(a).replace(sre, '') || '',
 		y = i(b).replace(sre, '') || '',
